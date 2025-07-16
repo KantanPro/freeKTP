a699a18e083ed297394cb9255fde62c3ebfa22e0
kantanpro <kantanpro22@gmail.com>
2025-07-16 14:19:23 +0900

    1.1.1(preview)リリース-2

--- ktpwp.php
+++ ktpwp.php
@@ -11,7 +11,7 @@
  * Text Domain: KantanPro
  * Domain Path: /languages
  * Requires at least: 5.0
- * Tested up to: 6.5
+ * Tested up to: 6.9.1
  * Requires PHP: 7.4
  * Update URI: https://github.com/KantanPro/freeKTP
  *
